960a495a2b59a4b134a695a661c165f15b08a0e8
Alerrando <alerrando2@gmail.com>
2023-12-16 16:14:35 -0300

    feat: Hello World index.html

--- src/main/java/com/example/messagingstompwebsocket/MessagingStompWebsocketApplication.java
+++ src/main/java/com/example/messagingstompwebsocket/MessagingStompWebsocketApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MessagingStompWebsocketApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MessagingStompWebsocketApplication.class, args);
 	}
